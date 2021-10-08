34586a10107bbaeae0c1c76349bb124ba0a57e55
Daniel050403 <89996090+Daniel050403@users.noreply.github.com>
2021-10-08 14:47:21 -0500

    Add files via upload

---
+++ 2Do parcial/Guardar primera letra.js
@@ -0,0 +1,2 @@
+var palabra= prompt("ingrese la palabra");
+document.write(("1") + palabra.substr(-200,1)); 
---
+++ 2Do parcial/funciones.js
@@ -0,0 +1,6 @@
+
+var palabra = prompt("Ingresa una palabra cualquiera");
+
+function arreglo (){
+    document.write(("1") + palabra.substr(-200,1)); 
+}
